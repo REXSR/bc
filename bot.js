0265dfb271f61ffcc352b0cdbb3a6011c0377f4b
REXSR <41483292+REXSR@users.noreply.github.com>
2018-09-24 04:24:16 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -193,7 +193,26 @@ if (message.content.startsWith("+content")) {
         }});
  
 
+  
+  client.on('ready', function(){
+    var ms = 1000 ;
+    var setGame = [`+h`,`+he`,`+hel`,`+help`,`i`,`in`,`inv`,`invi`,`invit`,`invite`];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/idk`);
+    }, ms);1000
 
+});
+  
+ 
 
 
 
